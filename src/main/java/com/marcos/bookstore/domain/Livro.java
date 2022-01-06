ddcde25a4394c6572e2a9a5e5407fcfe9dcfc807
MarcosRoch4 <in.cognitu@hotmail.com>
2022-01-06 15:16:41 -0300

    instancia de forma automática para a base de dados

--- src/main/java/com/marcos/bookstore/domain/Livro.java
+++ src/main/java/com/marcos/bookstore/domain/Livro.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Livro implements Serializable{
