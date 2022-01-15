82427829f112cdb2d89fedd4c77526a2b53f77c5
MarcosRoch4 <in.cognitu@hotmail.com>
2022-01-15 10:16:41 -0300

    POST para categoria

--- src/main/java/com/marcos/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/marcos/bookstore/resources/CategoriaResource.java
@@ -1,10 +1,8 @@
 package com.marcos.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.persistence.Entity;
-
 import com.marcos.bookstore.domain.Categoria;
 import com.marcos.bookstore.dtos.CategoriaDTO;
 import com.marcos.bookstore.service.CategoriaService;
@@ -13,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -37,4 +38,14 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(listDTO);
     }
 
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        //return ResponseEntity.created(uri).build(); pode ser utilizado dessa maneira tbm
+
+        return ResponseEntity.created(uri).body(obj); 
+
+
+    }
 }
